7514f051f2a45d59050a3ad5856525c5f9eaebfb
Philip Taffner <philip.taffner@bluegfx.de>
2012-12-28 00:35:02 +0100

    - fixed RequestDispatcher - activated full error reporting

--- server/index.php
+++ server/index.php
@@ -1,6 +1,7 @@
 <?php
 
 ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
 // set time zone to prevent php warnings
 date_default_timezone_set('Europe/Berlin');
--- server/lib/RequestDispatcher.class.php
+++ server/lib/RequestDispatcher.class.php
@@ -4,11 +4,11 @@ class RequestDispatcher {
 
 	public function __construct(){
 		
-		if(!preg_match('#'.BASE_URI.'/?([a-z]+)/(.+)#i', $_SERVER['REQUEST_URI'], $matches)){
+		if(!preg_match('#'.BASE_URI.'/?([a-z]*)(?:/(.*))?#i', $_SERVER['REQUEST_URI'], $matches)){
 			return;
 		}
 
-		define('REQUEST_URI', $matches[2]);
+		define('REQUEST_URI', isset($matches[2]) ? $matches[2] : '');
 
 		switch($matches[1]){
 			case 'api':
